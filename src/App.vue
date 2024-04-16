a5015c4509ee41de0068a4b4afa768839aac8c81
Cigno05 <paolocalcagni.82@gmail.com>
2024-04-16 12:06:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>ciao</h1>
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue'
+
+  export default {
+    components: {
+      AppHeader,
+    },
+    data() {
+      return {
+        
+      }
+    },
+    
+  }
+</script>
+
+<style lang="scss">
+@use './style/general';
+</style>
